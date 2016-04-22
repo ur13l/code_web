4556d753751549d82a6a46be983837fe8cc01104
Uriel <uriel@uriel-pc>
2016-04-22 09:50:11 -0500

    No sé

---
+++ src/app_php/login/enviarCorreo.php
@@ -0,0 +1,3 @@
+<?php
+mail("ur13l.infante@gmail.com","asuntillo","Este es el cuerpo del mensaje");
+?>
---
+++ src/app_php/login/recuperarContrasena.php
@@ -0,0 +1,29 @@
+<?php
+#Autor: Uriel Infante
+#Servicio para enviar un correo con el código de contraseña necesitado por el usuario.
+#Recibe mediante POST el parámetro correo.
+#Fecha: 21/04/2016
+
+include("../conexion/conexion.php");
+$conexion = connect();
+$correo = $_POST["correo"];
+$consulta = "select c.codigo from codigo_login_password c, login_app l WHERE l.correo =
+  '$correo' AND l.id_login_app = c.id_login_app; ";
+$result = mysqli_query($conexion, $consulta);
+$row = mysqli_fetch_array($result);
+
+if(isset($row)){
+  //Enviar correo
+  $row['status'] = "valid";
+  echo json_encode($row, true);
+}
+else{
+  $row = array();
+  $row['status'] = "invalid";
+  echo json_encode($row, true);
+}
+
+
+$conexion->close();
+
+ ?>
