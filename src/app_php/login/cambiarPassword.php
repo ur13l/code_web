daa4d61f5e5c340b67b9fe1111d815878d4f5f65
Uriel <uriel@uriel>
2016-04-28 13:24:23 -0500

    Cambiar contrasseña

---
+++ src/app_php/login/cambiarPassword.php
@@ -0,0 +1,27 @@
+<?php
+#Autor: Uriel Infante
+#Script para cambiar la contraseña de un nuevo usuario.
+#Recibe mediante POST el parámetro correo y la nueva contraseña.
+#Fecha: 27/04/2016
+
+include("../conexion/conexion.php");
+$conexion = connect();
+$correo = $_POST["correo"];
+$password = $_POST["password"];
+$consulta = "UPDATE login_app SET password = '$password' WHERE correo = '$correo';";
+
+
+if(!$result = mysqli_query($conexion, $consulta))
+{
+    echo "error";
+    $conexion->close();
+    exit();
+}
+else
+{
+    echo "success";
+}
+
+$conexion->close();
+
+ ?>
