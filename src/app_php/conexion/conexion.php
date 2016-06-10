a91795f9756c38bbd2400e25e6d1958dfc49282e
ur13l <ur13l.infante@gmail.com>
2016-06-10 01:53:48 -0500

    Gitignore 2

---
+++ src/app_php/conexion/conexion.php
@@ -0,0 +1,30 @@
+<?php
+
+#Autor: Uriel Infante
+#Archivo de conexión para el servidor de base de datos.
+#Todos los archivos que conecten a la base de datos deben hacer referencia
+#Fecha: 16/04/2016
+
+/**
+ * Función que revisa la conexión
+ * @return conexion: Devuelve la conexión para utilizarse en otro ámbito.
+ */
+function connect(){
+
+	$user = "root";
+	$password = "info2000";
+	$dbname = "code_web";
+	$port = 3306;
+	$host = "200.23.39.11"; //Ip Externa
+	//$host = 'localhost';
+	//$host = "10.0.7.40"; //Ip Interna
+// Create connection
+	$conn = mysqli_connect($host, $user, $password, $dbname);
+	// Check connection
+	if (!$conn) {
+	    echo "Connection failed: " . mysqli_connect_error();
+	}
+	return $conn;
+}
+
+?>
